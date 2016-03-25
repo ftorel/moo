cdc1757f245e55e17d865b84bca5cd3b486a015a
Florian TOREL <florian.torel@gmail.com>
2016-03-25 11:14:26 +0100

    test slack integration

--- src/TestServlet.java
+++ src/TestServlet.java
@@ -26,7 +26,7 @@ public class TestServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		
-		String flo = "Florian, tu sais faire un GIT ?";
+		String flo = "Pierre, tu sais faire un GIT ?";
 		
 		
 	}
